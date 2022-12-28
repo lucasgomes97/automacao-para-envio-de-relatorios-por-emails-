06ae0c6c774a89bcebe4c95c41cc75ae082bbe0a
Lucas Da Conceição Gomes <118493095+lucasgomes97@users.noreply.github.com>
2022-12-28 10:52:08 -0300

    Delete corrdenadas da tela.py

--- corrdenadas da tela.py
+++
@@ -1,4 +0,0 @@
-import pyautogui
-pyautogui.sleep(3)  #adiciona um deley em segundos apartir do valor indicado, para que tenhamos tempo para colocar o mouse onde queremos saber a coordenada
-print(pyautogui.size())     #informa a resolução da tela em x, y
-print(pyautogui.position()) #comando para informar as coordenadas da tela (x, y)
